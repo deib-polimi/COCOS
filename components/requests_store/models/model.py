328cb9831736f0edc3a20b1af9d8b3481c8ad598
Nicholas Rasi <nicholas.rasi@gmail.com>
2019-10-29 11:37:14 +0100

    Added profiled rt to model

--- components/requests_store/models/model.py
+++ components/requests_store/models/model.py
@@ -5,6 +5,7 @@ class Model:
                  version: int = None,
                  sla: float = None,
                  alpha: float = 1,
+                 profiled_rt: float = None,
                  json_data=None):
         if json_data:
             self.__dict__ = json_data
@@ -13,11 +14,13 @@ class Model:
             self.version = version
             self.sla = sla
             self.alpha = alpha
+            self.profiled_rt = profiled_rt
 
     def to_json(self):
         return {
             "name": self.name,
             "version": self.version,
             "sla": self.sla,
-            "alpha": self.alpha
+            "alpha": self.alpha,
+            "profiled_rt": self.profiled_rt
         }

f6f9b7f1f0231ef7859c6f9c13e5ec4a8430747a
Nicholas Rasi <nicholas.rasi@gmail.com>
2019-10-29 11:29:31 +0100

    Added control rules

--- components/controller/controller.py
+++ components/controller/controller.py
@@ -1,53 +1,128 @@
 import datetime
+import time
+
 import requests
 import logging
+import math
 from models.model import Model
 from models.container import Container
 from models.req import Req
 
 
 class Controller:
+    A1_NOM = 0.1963
+    A2_NOM = 0.002
+    A3_NOM = 0.5658
+    P_NOM = 0.4
 
-    def __init__(self, models_endpoint: str, containers_endpoint: str, requests_endpoint: str):
+    def __init__(self,
+                 models_endpoint: str,
+                 containers_endpoint: str,
+                 requests_endpoint: str,
+                 window_time: float,
+                 min_c: int,
+                 max_c: int):
         self.models_endpoint = models_endpoint
         self.containers_endpoint = containers_endpoint
         self.requests_endpoint = requests_endpoint
+        self.window_time = window_time
 
-        self.models = []
+        # min and max core allocation
+        self.min_c = min_c
+        self.max_c = max_c
+
+        self.models = {}
+        self.ui_old = {}
         self.containers = []
 
         self.logs = []
 
     def init(self):
         # get the models
-        self.models = [Model(json_data=json_model)
-                       for json_model in self.get_data(self.models_endpoint)]
-        self.logs.append("Loaded " + str(len(self.models)) + " models: " + str(
-            [model.to_json() for model in self.models]))
+        self.models = {json_model["name"]: Model(json_data=json_model)
+                       for json_model in self.get_data(self.models_endpoint)}
+        self.ui_old = {model: 0.0 for model in list(self.models.keys())}
+        log_str = "Loaded " + str(len(self.models)) + " models: " + str(
+            [model for model in self.models])
+        self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
 
         # get the containers
         self.containers = [Container(json_data=json_container)
                            for json_container in self.get_data(self.containers_endpoint)]
-        self.logs.append("Loaded " + str(len(self.containers)) + " containers: " + str(
-            [container.to_json() for container in self.containers]))
+        log_str = "Loaded " + str(len(self.containers)) + " containers: " + str(
+            [container.to_json() for container in self.containers])
+        self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
 
     def update(self):
-        # get the requests data
-        self.logs.append(str(datetime.datetime.now()) + " / Getting the request")
-        reqs = [Req(json_data=json_req) for json_req in self.get_data(self.requests_endpoint)]
+        # get the metrics data since from_ts
+        from_ts = time.time() - self.window_time
+        metrics = self.get_data(self.requests_endpoint + '/metrics/model', {'from_ts': from_ts})
+        log_str = "Got {} metrics from {}".format(len(metrics), str(datetime.datetime.fromtimestamp(from_ts)))
+        self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
 
-        # apply control given models and containers
-        self.logs.append(str(datetime.datetime.now()) + " / Applying control")
+        for metric in metrics:
+            # get response time for the model
+            rt = metric["metrics"]["avg"]
+            reqs_number = metric["metrics"]["created"]
 
-        # update containers
-        self.logs.append(str(datetime.datetime.now()) + " / Update containers")
+            # apply control given models and containers
+            log_str = 'Applying control for model: {}<br/>' \
+                      'current_avg_rt: {}<br/>current_created_num_reqs: {}'.format(metric["model"],
+                                                                   rt,
+                                                                   reqs_number)
+
+            # check that some metrics exists
+            if rt is None or reqs_number is None:
+                log_str = log_str + "<br>Skip {}: model metrics not found".format(metric["model"])
+                self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
+                continue
+
+            # get model data
+            model = self.models[metric["model"]]
+            if not model:
+                log_str = log_str + "<br>Skip {}: model data not found".format(metric["model"])
+                self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
+                continue
+
+            sla = model.sla
+            alpha = model.alpha
+            ui_old = self.ui_old[model.name]
+
+            e = sla - rt
+            ke = (alpha - 1) / (self.P_NOM - 1) * e
+            ui = ui_old + (1 - self.P_NOM) * ke
+            ut = ui + ke
+
+            core = reqs_number * (ut - self.A1_NOM - 1000.0 * self.A2_NOM) / (
+                    1000.0 * self.A3_NOM * (self.A1_NOM - ut))
+
+            approx_core = math.ceil(min(self.max_c, max(core, self.min_c)))
+
+            approx_ut = ((1000.0 * self.A2_NOM + self.A1_NOM) * reqs_number +
+                         1000.0 * self.A1_NOM * self.A3_NOM * approx_core) / (
+                                reqs_number + 1000.0 * self.A3_NOM * approx_core)
+
+            log_str = log_str + "<br>sla: {2:.4f}<br/>" \
+                                "error: {3:.4f}<br/>" \
+                                "ke: {4:.4f}<br/>" \
+                                "ui, ui_old, ut, approx_ut: {5:.4f} {6:.4f} {7:.4f} {8:.4f}<br/>" \
+                                "core, approx_core: {9:.4f} {10:.4f}".format(
+                rt, reqs_number, sla, e, ke, ui, ui_old, ut, approx_ut, core, approx_core)
+
+            self.ui_old[model.name] = approx_ut - ke
+
+            allocation = approx_core
+
+            # update containers
+            log_str = log_str + "<br>Model {}: allocation: {}, updating containers...".format(model.name, allocation)
+            self.logs.append({"ts": time.time(), "date": str(datetime.datetime.now()), "msg": log_str})
 
     def get_logs(self):
         return self.logs
 
-    def get_data(self, url):
+    def get_data(self, url, data=None):
         try:
-            response = requests.get(url)
+            response = requests.get(url, params=data)
         except Exception as e:
             logging.warning(e)
             response = []
--- components/controller/main.py
+++ components/controller/main.py
@@ -1,11 +1,12 @@
 import argparse
 import logging
-import threading
-
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, jsonify
+from flask_cors import CORS
 from controller import Controller
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
@@ -19,32 +20,47 @@ def get_logs():
 
 
 def control():
-    app.logger.info("Control")
+    app.logger.info("Controller updating...")
     controller.update()
-    threading.Timer(args.time, control).start()
+    app.logger.info("Controller updated, waiting for next clock...")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--containers_manager', type=str, required=True)
     parser.add_argument('--requests_store', type=str, required=True)
+    parser.add_argument('--min_c', type=int, required=True)
+    parser.add_argument('--max_c', type=int, required=True)
     parser.add_argument('--time', type=float, required=True)
     args = parser.parse_args()
+
+    # init log
+    format = "%(threadName)s:%(asctime)s: %(message)s"
+    logging.basicConfig(format=format,
+                        level=logging.INFO,
+                        datefmt="%H:%M:%S")
 
     models_endpoint = args.containers_manager + "/models"
     logging.info("Setting models manager to: %s", models_endpoint)
     containers_endpoint = args.containers_manager + "/containers"
     logging.info("Setting containers_manager to: %s", containers_endpoint)
-    requests_endpoint = args.requests_store + "/requests"
+    requests_endpoint = args.requests_store
     logging.info("Setting requests_store to: %s", requests_endpoint)
 
     status = "init controller"
     logging.info(status)
-    controller = Controller(models_endpoint, containers_endpoint, requests_endpoint)
+    controller = Controller(models_endpoint,
+                            containers_endpoint,
+                            requests_endpoint,
+                            args.time,
+                            args.min_c,
+                            args.max_c)
     controller.init()
 
-    threading.Timer(args.time, control).start()
+    sched = BackgroundScheduler()
+    sched.add_job(control, 'interval', seconds=args.time)
+    sched.start()
 
     status = "running"
     logging.info(status)
-    app.run(port=5003, debug=True)
+    app.run(port=5003, debug=False)
